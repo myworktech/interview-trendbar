6f0237e3ffff559b47af5a6f61fd9119644dbfd9
Ilya <myworktech@gmail.com>
2019-10-20 12:21:10 +0300

    * test renamed

--- src/test/java/com/myworktech/trendbar/unit/QuoteHandlerTypeTest.java
+++ src/test/java/com/myworktech/trendbar/unit/QuoteHandlerTypeTest.java
@@ -15,7 +15,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class QuoteHandlerTest {
+public class QuoteHandlerTypeTest {
 
     @Test
     public void testSameInstance() throws Throwable {
